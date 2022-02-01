db83fe1cab96bfcd9f5c4db6312572ab32bcc762
Yabadaba <allenbao01@gmail.com>
2022-01-31 23:15:05 -0800

    bruh

--- Assignment5.java
+++ Assignment5.java
@@ -5,11 +5,15 @@
 // This program finds the number of permutations with a recursive method
 
 public class Assignment5 {
-    public static int permut(int n, int r) {
+    public static int permut(int n, int r) throws IllegalArgumentException{
         // essentially, we keep multiplying n by n-1 an r amount of times, hence we can use r as our counter.
-        // after an r - 1 amount of times, we break out the loop
-        if (r == 1) {
-            return n;
+        // after an r amount of times, we break out the loop
+        if (n < r) {
+            throw new IllegalArgumentException(String.format("n must be greater than or equal to r," +
+                    " but n = %1d and r = %2d", n, r));
+        }
+        if (r == 0) {
+            return 1;
         }
         // we call the method again with n-1 and r-1 and be sure to multiply it every time.
         return (n * permut(n-1, r-1));
@@ -18,6 +22,7 @@ public class Assignment5 {
     // main method
     public static void main(String[] args) {
         // test cases
+        // System.out.println(permut(0, 5));
         System.out.println(permut(5, 1));
         System.out.println(permut(55, 5));
     }
