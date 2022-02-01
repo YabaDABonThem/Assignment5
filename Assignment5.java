c46b06909f935db57c8cd6d2e76b3525b2d3bdf6
Yabadaba <allenbao01@gmail.com>
2022-01-31 23:18:50 -0800

    finally

--- Assignment5.java
+++ Assignment5.java
@@ -7,12 +7,13 @@
 public class Assignment5 {
     public static int permut(int n, int r) throws IllegalArgumentException{
         // essentially, we keep multiplying n by n-1 an r amount of times, hence we can use r as our counter.
-        // after an r amount of times, we break out the loop
         if (n < r) {
+            // throw an exception when n is greater than r, because that is an invalid input.
             throw new IllegalArgumentException(String.format("n must be greater than or equal to r," +
                     " but n = %1d and r = %2d", n, r));
         }
         if (r == 0) {
+            // after an r amount of times, we break out the loop
             return 1;
         }
         // we call the method again with n-1 and r-1 and be sure to multiply it every time.
